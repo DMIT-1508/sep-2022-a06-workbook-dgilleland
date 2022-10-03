6b0a14e5b82acbc5ca1a20bdae7b58d35d58ee7c
Dan Gilleland <DGilleland@nait.ca>
2022-10-03 14:44:49 -0600

    end of class - SQL intro

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -12,9 +12,23 @@ BEGIN
     CREATE DATABASE [SchoolTranscript]
 END
 GO
+-- Single line comments start with two dashes
+-- DROP DATABASE [SchoolTranscript]
 
 -- Switch execution context to the database
 USE [SchoolTranscript] -- remaining SQL statements will run against the SchoolTranscript database
 GO
 
 -- Create Tables...
+-- The CREATE TABLE statement contains a comma-separated list
+-- of column declarations
+-- DROP TABLE Students
+CREATE TABLE Students
+(
+    [StudentID]       int,
+    [GivenName]       varchar(50),
+    [Surname]         varchar(50),
+    [DateOfBirth]     datetime,
+    [Enrolled]        bit
+)
+
