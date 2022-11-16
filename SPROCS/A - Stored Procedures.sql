3a4f5d781ea868663816d26106b9b861ddc5df6e
Dan Gilleland <dgilleland@nait.ca>
2022-11-16 17:47:00 +0000

    Nov 16, 2022, 10:47 AM

--- SPROCS/A - Stored Procedures.sql
+++ SPROCS/A - Stored Procedures.sql
@@ -9,10 +9,7 @@ GO
     so place a GO statement in-between each question to execute 
     the previous batch (question) and start another.
 
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'SprocName')
-    DROP PROCEDURE SprocName
-GO
-CREATE PROCEDURE SprocName
+CREATE OR ALTER PROCEDURE SprocName
     -- Parameters here
 AS
     -- Body of procedure here
